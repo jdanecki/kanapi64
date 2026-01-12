1755fc35599045b797b321a396612cbb5f53e7ee
Jacek Danecki <jacek.m.danecki@gmail.com>
2026-01-12 19:59:54 +0100

    simple test for clang

--- kanapi_system/tests/test_libstdcpp.cpp
+++ kanapi_system/tests/test_libstdcpp.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <cstdlib>
+#include <stddef.h>
 
 int main(int argc, char * argv[])
 {
 	if (argv[0] != nullptr)
-		std::cout << "test for libstdc++\n";
+    {
+		std::cout << "test for libstdc++  \n";
+    }
+    int a=EXIT_SUCCESS;
+    std::cout << "a=" << a << std::endl;
 	return 0;
 }
 
